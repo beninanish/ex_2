58954266471615025d89e0e360ff6d6228441217
ITLAB <ITLAB@IT208>
2017-01-31 16:18:07 +0530

    Second commit with all the operations done.

--- src/com/example/native_cal/MainActivity.java
+++ src/com/example/native_cal/MainActivity.java
@@ -1,35 +1,71 @@
 package com.example.native_cal;
 
 import android.support.v7.app.ActionBarActivity;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView; 
 
 
-public class MainActivity extends ActionBarActivity {
-
+@SuppressLint("NewApi") public class MainActivity extends ActionBarActivity {
+	Button plus,minus,mul,div;
+	EditText t1,t2;
+	TextView res;
+	double num1,num2,sum;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+        
+        t1 = (EditText)findViewById(R.id.editText1);
+        t2 = (EditText)findViewById(R.id.editText2);
+        res = (TextView)findViewById(R.id.textView1);
+        plus = (Button)findViewById(R.id.plus);
+        minus = (Button)findViewById(R.id.minus);
+        mul= (Button)findViewById(R.id.mul);
+        div = (Button)findViewById(R.id.div);
+        
+        plus.setOnClickListener(new OnClickListener() {
+            
+            public void onClick(View v) {
+                num1 = Double.parseDouble(t1.getText().toString());
+                num2 = Double.parseDouble(t2.getText().toString());
+                sum = num1 + num2;
+                res.setText(Double.toString(sum));
+            }
+        });
+        
+        minus.setOnClickListener(new OnClickListener() {
+            
+            public void onClick(View v) {
+                num1 = Double.parseDouble(t1.getText().toString());
+                num2 = Double.parseDouble(t2.getText().toString());
+                sum = num1 - num2;
+                res.setText(Double.toString(sum));
+            }
+        });
+        mul.setOnClickListener(new OnClickListener() {
+            
+            public void onClick(View v) {
+                num1 = Double.parseDouble(t1.getText().toString());
+                num2 = Double.parseDouble(t2.getText().toString());
+                sum = num1 * num2;
+                res.setText(Double.toString(sum));
+            }
+        });
+        div.setOnClickListener(new OnClickListener() {
+            
+            public void onClick(View v) {
+                num1 = Double.parseDouble(t1.getText().toString());
+                num2 = Double.parseDouble(t2.getText().toString());
+                sum = num1 / num2;
+                res.setText(Double.toString(sum));
+            }
+        });
+    } 
 }
